fdeb885c710cbf6a2a5fcbbb9d7d1e9f87fe78f4
arc360alt <voidyt16@gmail.com>
2026-05-20 23:14:14 -0500

    gnhfpkoj

--- api/index.js
+++ api/index.js
@@ -2,7 +2,15 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express()
-app.use(cors({ origin: ['https://weather.arc360hub.com', 'http://localhost:5173'] }))
+app.use(cors({
+  origin: (origin, cb) => {
+    if (!origin || /^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/.test(origin) || origin.endsWith('.arc360hub.com') || origin === 'https://arc360hub.com') {
+      cb(null, true)
+    } else {
+      cb(new Error('CORS: origin not allowed'))
+    }
+  },
+}))
 app.use(express.json({ limit: '2mb' }))
 
 const GOOGLE_KEY   = process.env.GOOGLE_API_KEY || ''
